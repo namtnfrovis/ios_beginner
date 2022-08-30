e03bba227050ef81af37cf3d5fad7c13fe29e79f
Tran Nhat Nam <namtn.frovis@gmail.com>
2022-08-30 11:31:12 +0700

    first commit

---
+++ ios_beginning_tutorial/Lession_1/Model/MockupPetData.swift
@@ -0,0 +1,21 @@
+//
+//  MockupPetData.swift
+//  ios_beginning_tutorial
+//
+//  Created by Tran Nhat Nam on 30/08/2022.
+//
+
+import Foundation
+ let petsData = [
+    Pet(name: "Cat", title: "It is lovely cat 1"),
+    Pet(name: "Cat", title: "It is lovely cat 2"),
+    Pet(name: "Cat", title: "It is lovely cat 3"),
+    Pet(name: "Cat", title: "It is lovely cat 4"),
+    Pet(name: "Cat", title: "It is lovely cat 5"),
+    
+    Pet(name: "Dog", title: "It is lovely dog 1"),
+    Pet(name: "Dog", title: "It is lovely dog 2"),
+    Pet(name: "Dog", title: "It is lovely dog 3"),
+    Pet(name: "Dog", title: "It is lovely dog 4"),
+    Pet(name: "Dog", title: "It is lovely dog 5"),
+ ]
---
+++ ios_beginning_tutorial/Lession_1/Model/Pet.swift
@@ -0,0 +1,18 @@
+//
+//  Pet.swift
+//  ios_beginning_tutorial
+//
+//  Created by Tran Nhat Nam on 30/08/2022.
+//
+
+import Foundation
+
+struct Pet {
+    var name: String?
+    var title: String?
+    
+    init(name: String?, title: String?) {
+        self.name = name
+        self.title = title 
+    }
+}
